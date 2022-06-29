3281d8abaaa9059740ef357656891f71cefeffa1
GHTeodor <Theodor.Lviv@gmail.com>
2022-06-29 15:25:38 +0300

    @OneToOne Unidirectional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import models.Gender;
+import models.Passport;
 import models.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -5,6 +7,9 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +20,7 @@ public class Main {
 
         Metadata metadata = new MetadataSources(serviceRegistry)
                 .addAnnotatedClass(User.class)
-//                .addAnnotatedClass(Passport.class)
+                .addAnnotatedClass(Passport.class)
                 .getMetadataBuilder()
                 .build();
 
@@ -27,9 +32,35 @@ public class Main {
 
         session.beginTransaction();
 
-
+        session.save(new User("ananas", new Passport("SI-76543"), Gender.MALE, Arrays.asList("Java", "JS")));
+        session.save(new User("banana", new Passport("SI-00001"), Gender.FEMALE, Arrays.asList("Java", "HTML")));
+        session.save(new User("cherry", new Passport("SI-67894"), Gender.MALE, Arrays.asList("Java", "C#")));
+        session.save(new User("tomato", new Passport("SI-12355"), Gender.FEMALE, Arrays.asList("Java", "Mongo")));
+        session.save(new User("papaya", new Passport("SI-55512"), Gender.MALE, Arrays.asList("Java", "MySQL")));
 
         session.getTransaction().commit();
+
+//        List<User> users = session.createNativeQuery("select * from  user_table", User.class).getResultList();
+//        List<User> users = session.createQuery("select u from  User u", User.class).getResultList();
+//        List<User> users = session
+//                .createQuery("select u from  User u where  u.id>:id and u.name!=:name", User.class)
+//                .setParameter("id", 2)
+//                .setParameter("name", "banana")
+//                .getResultList();
+//        System.out.println(users);
+
+//        User user = session.find(User.class, 1);
+//        System.out.println(user);
+//        user.setName("new fruit1");
+//
+        session.beginTransaction();
+////        session.update(user);
+//        session.delete("user_table", user);
+
+        session.getTransaction().commit();
+
+        session.createQuery("select u from User u", User.class).getResultList().forEach(System.out::println);
+
         session.close();
         sessionFactory.close();
     }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,25 +1,57 @@
 package models;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "user_table")
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
+@ToString(exclude = {"passport"})
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "username")
     private String name;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+//    @PrimaryKeyJoinColumn
+    @JoinColumn(name = "passport_id")
+    private Passport passport;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ElementCollection
+    private List<String> skills = new ArrayList<>();
+
+    public User() {
+    }
 
     public User(String name) {
         this.name = name;
+    }
+
+    public User(String name, Passport passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    public User(String name, Passport passport, Gender gender) {
+        this.name = name;
+        this.passport = passport;
+        this.gender = gender;
+    }
+
+    public User(String name, Passport passport, Gender gender, List<String> skills) {
+        this.name = name;
+        this.passport = passport;
+        this.gender = gender;
+        this.skills = skills;
     }
 }
